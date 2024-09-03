5d586b1ec1c043c3e8c15252393d9eebe212d243
Peter Boldarev <pitryxa@gmail.com>
2024-09-03 15:03:37 +0200

    Update App.java

--- src/main/java/pitryxa/App.java
+++ src/main/java/pitryxa/App.java
@@ -13,23 +13,32 @@ import pitryxa.action.scroll.ScrollNumber;
 
 import java.awt.AWTException;
 import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Point;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class App {
+    private static final Integer DEFAULT_WIDTH = 1920;
+    private static final Integer DEFAULT_HEIGHT = 1080;
+
     public static void main(String[] args) {
         try {
             final MouseMotionFactory motionFactory = getMotionFactory();
             InitialMouseArea initialMouseArea = new InitialMouseArea(motionFactory, 300);
+            Cursor cursor = new Cursor(motionFactory);
             new MouseImitation(
                     new Mouse(
                             new Motion(
-                                    new Cursor(motionFactory),
+                                    cursor,
                                     new Offset(initialMouseArea, 20, 20)),
                             new Scroll(
                                     new Delay(100, 400),
                                     new ScrollNumber(-10, 10)),
                             new Click(
-                                    new Cursor(motionFactory),
+                                    cursor,
                                     initialMouseArea,
                                     new Delay(100, 300),
                                     new Delay(1_000, 10_000))),
@@ -41,10 +50,19 @@ public class App {
     }
 
     private static MouseMotionFactory getMotionFactory() {
-        Dimension screenOnTheRight = new Dimension(1920, 1080);
-        Dimension mainScreen = new Dimension(1920, 1080);
+        GraphicsDevice[] screenDevices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+        Integer width = Arrays.stream(screenDevices)
+                .map(GraphicsDevice::getDisplayMode)
+                .map(DisplayMode::getWidth)
+                .reduce(Integer::sum)
+                .orElse(DEFAULT_WIDTH);
+        Integer height = Arrays.stream(screenDevices)
+                .map(GraphicsDevice::getDisplayMode)
+                .map(DisplayMode::getHeight)
+                .min(Comparator.comparingInt(Integer::intValue))
+                .orElse(DEFAULT_HEIGHT);
         ScreenAdjustedNature nature = new ScreenAdjustedNature(
-                new Dimension(screenOnTheRight.width + mainScreen.width, 1080),
+                new Dimension(width, height),
                 new Point(0, 0)
         );
         return FactoryTemplates.createAverageComputerUserMotionFactory(nature);
